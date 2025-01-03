a69213786d23a388ae33a22a6117538c68914d72
Aleksandr Lizin <sogimu@nxt.ru>
2025-01-04 02:00:24 +0700

    *Clenup main.cpp with test programm. Now programm has function to calculate fibonacci n's number

--- parser/src/main.cpp
+++ parser/src/main.cpp
@@ -11,65 +11,23 @@ int main()
    // 23's number shall be 17711
    std::string e = R"(
     {
-      // a = 5;
-      // b = 42+3;
-      // print(b);
-      // if( 1< 2 )
-      // {
-      //    a = b;
-      // }
-      // foo(n)
-      // {
-      //   a = 0;
-      //   b = 1;
-      //   i = 2;
-      //   while( i < n )
-      //   {
-      //       c = a + b;
-      //       a = b;
-      //       b = c;
-      //       i = i + 1;
-      //   }
-      //   print(b);
-      //   print(b==17711);
-      //   return b;
-      //   print(123);
-      //   print(11111);
-      // }
-      // n = 23;  
-      // a = 0;
-      // b = 1;
-      // i = 2;
-      // while( i < n )
-      // {
-      //     c = a + b;
-      //     a = b;
-      //     b = c;
-      //     i = i + 1;
-      // }
-      // print(b);
-      // print(b==17711);
-      // foo(23);
-      foo(x)
-    {
-      return x*x;
-    }
-      bar(a)
+      fibonacci(n)
       {
-        // print(a);
-        // if(a > 0)
-        // {
-        //   bar( a - 1 );
-        // }
-        // print( a );
-        // return a;
-        return foo(a);
+        a = 0;
+        b = 1;
+        i = 2;
+        while( i < n )
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            i = i + 1;
+        }
+        return b;
       }
-      print(bar(5));
-      // b = bar(3);
-      // print( b );
-
-      // print( bar(2) );
+      number = fibonacci(23);
+      print(number);
+      print(number==17711);
     })";
    //
    //        if(3 < 2)
