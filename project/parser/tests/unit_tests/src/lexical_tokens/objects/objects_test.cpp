d63f523b9875171dc8878554a6e2b388f939f443
Aleksandr Lizin <sogimu@nxt.ru>
2026-01-16 13:51:05 +0700

    * Extend test for class LexicalTokens for case with object

--- project/parser/tests/unit_tests/src/lexical_tokens/objects/objects_test.cpp
+++ project/parser/tests/unit_tests/src/lexical_tokens/objects/objects_test.cpp
@@ -8,7 +8,7 @@ TEST( LEXICAL_TOKENS_OBJECTS, DEFINITION )
 {
    // ARRANGE
    const auto& input = R"""(
-    {"key": 12}
+    {"key": 12, "key1": 42}
   )""";
 
    // ACT
@@ -25,10 +25,18 @@ TEST( LEXICAL_TOKENS_OBJECTS, DEFINITION )
    EXPECT_EQ( lexical_tokens[3], t3 );
    LexicalTokens::LexicalToken t4{ .text="12", .type=Token_Type::INT, .line=1, .col=12, .length=2 };
    EXPECT_EQ( lexical_tokens[4], t4 );
-   LexicalTokens::LexicalToken t5{ .text="}", .type=Token_Type::CLOSE_CURLY_BRACKET, .line=1, .col=14, .length=1 };
+   LexicalTokens::LexicalToken t5{ .text=",", .type=Token_Type::COMMA, .line=1, .col=14, .length=1 };
    EXPECT_EQ( lexical_tokens[5], t5 );
-   LexicalTokens::LexicalToken t6{ .text="", .type=Token_Type::EOL, .line=0, .col=0, .length=0 };
+   LexicalTokens::LexicalToken t6{ .text="key1", .type=Token_Type::STRING, .line=1, .col=16, .length=4 };
    EXPECT_EQ( lexical_tokens[6], t6 );
+   LexicalTokens::LexicalToken t7{ .text=":", .type=Token_Type::COLON, .line=1, .col=22, .length=1 };
+   EXPECT_EQ( lexical_tokens[7], t7 );
+   LexicalTokens::LexicalToken t8{ .text="42", .type=Token_Type::INT, .line=1, .col=24, .length=2 };
+   EXPECT_EQ( lexical_tokens[8], t8 );
+   LexicalTokens::LexicalToken t9{ .text="}", .type=Token_Type::CLOSE_CURLY_BRACKET, .line=1, .col=26, .length=1 };
+   EXPECT_EQ( lexical_tokens[9], t9 );
+   LexicalTokens::LexicalToken t10{ .text="", .type=Token_Type::EOL, .line=0, .col=0, .length=0 };
+   EXPECT_EQ( lexical_tokens[10], t10 );
 }
 
 TEST( LEXICAL_TOKENS_OBJECTS, SUBSCRIPT_OPERATOR )
